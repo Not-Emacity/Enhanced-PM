ccc181f281ee8ff5678ca61c45835ac582ac6c18
Matthew <54685753+Not-Emacity@users.noreply.github.com>
2023-09-05 23:08:10 -0500

    Updated usModelFill
    -Fixed a bug where you couldn't select generations
    -Fixed a bug where New Letter Combination was added to formats that did not have tables
    -Added support for states with multiple tables

--- Scripts/usModelFill.js
+++ Scripts/usModelFill.js
@@ -1,29 +1,88 @@
-document.querySelector("#frm > fieldset:nth-child(3) > div:nth-child(3) > section:nth-child(1)").innerHTML += '<button class="btn-u" type="button" onclick="runPlate();">Run Plate</button>';
+document.querySelector("#frm > fieldset:nth-child(3) > section:nth-child(1)").outerHTML += '<button class="btn-u" type="button" onclick="runPlate();">Run Plate</button>';
+const standard = ["ABC123", "123ABC"];
+const fourDigit = ["ABC1234", "1234ABC"];
 
-const standardForm = new Set([
-	"TX",
-	"AK",
-	"HI",
-	"MS",
-	"AZ",
-	"MT",
-	"NY",
-	"VT",
-	"IL",
-	"OH",
-	"NC",
-	"MD",
-	"PA"
+const singleFormats = new Map([
+	["AK", "ABC123"],
+	["HI", "ABC123"],
+	["MT", "ABC123"],
+	["VT", "ABC123"],
+	["TX", "ABC1234"],
+	["MS", "ABC1234"],
+	["AZ", "ABC1234"],
+	["NY", "ABC1234"],
+	["OH", "ABC1234"],
+	["NC", "ABC1234"],
+	["PA", "ABC1234"],
+	["MD", "1AB2345"],
+	["IL", "X12 3456"],
+	["AR", "123ABC"],
+	["CT", "123ABC"],
+	["ME", "1234AB"],
+	["DC", "AB1234"],
+	["CA", "1ABC234"]
 ]);
 
-const twoDigit = new Set([
-	"DC",
-	"ME"
+const multiFormats = new Map([
+	["OK", standard],
+	["UT", ["A123BC", "123ABC"]],
+	["LA", standard],
+	["NM", standard],
+	["KS", standard],
+	["ND", standard],
+	["MN", standard],
+	["IA", standard],
+	["CO", standard],
+	["OR", standard],
+	["SC", standard],
+	["GA", fourDigit],
+	["VA", fourDigit],
+	["MI", ["ABC123", "ABC1234"]],
+	["NE", ["ABC123", "123AB"]],
+	//Separate tables for 123ABC hyphenated
+	["NV", ["ABC123", "123ABC", "12A345", "123A45"]],
+	["IN", ["ABC123", "123ABC", "123AB", "123A"]],
+	//Separate tables for ABC123 hyphenated
+	["KY", ["ABC123", "123ABC", "A1B234"]]
 ]);
 
-const latterThree = new Set([
-	"CT",
-	"AR"
+const sixDigit = new Map([
+	["OK", "1"],
+	["LA", "151"],
+	["NM", "183"],
+	["KS", "3402"],
+	["ND", "4102"],
+	["MN", "171"],
+	["IA", "191"],
+	["CO", "132"],
+	["OR", "401"],
+	["SC", "2301"],
+	["MI", "82"],
+	["NE", "3601"],
+	["NV", "204"],
+	["IN", "604"]
+]);
+
+const sixDigitFlip = new Map([
+	["OK", "2"],
+	["UT", "2"],
+	["LA", "152"],
+	["NM", "181"],
+	["KS", "3401"],
+	["ND", "4101"],
+	["MN", "172"],
+	["IA", "192"],
+	["CO", "133"],
+	["OR", "402"],
+	["SC", "2302"],
+	["KY", "161"],
+	["IN", "601"]
+]);
+
+const sevenDigit = new Map([
+	["GA", "91"],
+	["VA", "61"],
+	["MI", "83"]
 ]);
 
 async function runPlate() {
@@ -125,15 +184,113 @@ async function decode(state, plate) {
 }
 
 async function newCombo(state, plate) {
-  let url;
-  if (standardForm.has(state)) {
-  	url = 'https://platesmania.com/us/result_' + state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(1,1) + '&b3=' + plate.substr(2,1) + '&posted=1&Submit=';
-  } else if (twoDigit.has(state)) {
-    url = 'https://platesmania.com/us/result_' + state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(1,1) + '&posted=1&Submit=';
-  } else if (state == "CA") {
-  	url = 'https://platesmania.com/us/result_' + state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(1,1) + '&b3=' + plate.substr(2,1) + '&b4=' + plate.substr(3,1) + '&posted=1&Submit=';
-  } else if (latterThree.has(state)) {
-  	url = 'https://platesmania.com/us/result_' + state.toLowerCase() + '.php?b1=' + plate.substr(4,1) + '&b2=' + plate.substr(5,1) + '&b3=' + plate.substr(6,1) + '&posted=1&Submit=';
+  let url = 'https://platesmania.com/us/result_';
+  if (singleFormats.has(state)) {
+  	let form = checkFormat(plate, singleFormats.get(state));
+  	if (form) {
+			switch (form) {
+  			case 1:
+  			case 2:
+  			case 7:
+  			case 8:
+  				url =+ state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(1,1) + '&b3=' + plate.substr(2,1) + '&posted=1&Submit=';
+  				break;
+  			case 4:
+  				url =+ state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(1,1) + '&posted=1&Submit=';
+  				break;
+  			case 6:
+  				url =+ state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(1,1) + '&b3=' + plate.substr(2,1) + '&b4=' + plate.substr(3,1) + '&posted=1&Submit=';
+  				break;
+  			case 3:
+  				if (/[-\s]/.test(plate)) {
+  					url =+ state.toLowerCase() + '.php?b1=' + plate.substr(4,1) + '&b2=' + plate.substr(5,1) + '&b3=' + plate.substr(6,1) + '&posted=1&Submit=';
+  				} else {
+  					url =+ state.toLowerCase() + '.php?b1=' + plate.substr(3,1) + '&b2=' + plate.substr(4,1) + '&b3=' + plate.substr(5,1) + '&posted=1&Submit=';
+  				}
+  				break;
+  			case 5:
+  				if (/[-\s]/.test(plate)) {
+  					url =+ state.toLowerCase() + '.php?b1=' + plate.substr(5,1) + '&b2=' + plate.substr(6,1) + '&posted=1&Submit=';
+  				} else {
+  					url =+ state.toLowerCase() + '.php?b1=' + plate.substr(4,1) + '&b2=' + plate.substr(5,1) + '&posted=1&Submit=';
+  				}
+  				break;
+  			default:
+  				break;
+  		}
+  	}
+  } else if (multiFormats.has(state)) {
+  	let type = "&tip=";
+  	let form = checkFormats(plate, multiFormats.get(state));
+  	switch (form) {
+  		case 1:
+  			if (state == "KY") {
+  				if (isHyphen(plate)) {
+  					type += "168";
+  				} else {
+  					type += "3204";
+  				}
+  			} else {
+  				type += sixDigit.get(state);
+  			}
+  			url += state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(1,1) + '&b3=' + plate.substr(2,1) + type + '&posted=1&Submit=';
+  			break;
+  		case 2:
+  			if (state == "NV") {
+  				if (isHyphen(plate)) {
+  					type += "201"
+  				} else {
+  					type += "205"
+  				}
+  			} else {
+  				type += sixDigitFlip.get(state);
+  			}
+  			if (isHyphen(plate)) {
+  				url += state.toLowerCase() + '.php?b1=' + plate.substr(4,1) + '&b2=' + plate.substr(5,1) + '&b3=' + plate.substr(6,1) + type + '&posted=1&Submit=';
+  			} else {
+  				url += state.toLowerCase() + '.php?b1=' + plate.substr(3,1) + '&b2=' + plate.substr(4,1) + '&b3=' + plate.substr(5,1) + type + '&posted=1&Submit=';
+  			}
+  			break;
+  		case 3:
+  			type += sevenDigit.get(state);
+				url += state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(1,1) + '&b3=' + plate.substr(2,1) + type + '&posted=1&Submit=';
+  			break;
+  		case 4:
+  			if (state == "VA") {
+  				type += "63";
+  			} else if (state == "GA") {
+  				type += "93";
+  			}
+  			url += state.toLowerCase() + '.php?b1=' + plate.substr(5,1) + '&b2=' + plate.substr(6,1) + '&b3=' + plate.substr(7,1) + type + '&posted=1&Submit=';
+  			break;
+  		case 5:
+  			if (state == "NE") {
+  				type += "3603";
+  			} else if (state == "IN") {
+  				type += "602";
+  			}
+  			url += state.toLowerCase() + '.php?b1=' + plate.substr(3,1) + '&b2=' + plate.substr(4,1) + type + '&posted=1&Submit=';
+  			break;
+  		case 6:
+  			type += "603";
+  			url += state.toLowerCase() + '.php?b1=' + plate.substr(3,1) + type + '&posted=1&Submit=';
+  			break;
+  		case 7:
+  			type += "3203";
+  			url += state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(1,1) + '&b3=' + plate.substr(2,1) + type + '&posted=1&Submit=';
+  			break;
+  		case 8:
+  			type += "1";
+  			url += state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(5,1) + '&b3=' + plate.substr(6,1) + type + '&posted=1&Submit=';
+  		case 9:
+  			type += "202";
+  			url += state.toLowerCase() + '.php?b1=' + plate.substr(0,1) + '&b2=' + plate.substr(1,1) + '&b3=' + plate.substr(2,1) + type + '&posted=1&Submit=';
+  			break;
+  		case 10:
+  			type += "203";
+  			url += state.toLowerCase() + '.php?b1=' + plate.substr(4,1) + '&b2=' + plate.substr(5,1) + '&b3=' + plate.substr(6,1) + type + '&posted=1&Submit=';
+  			break;
+  	}
   }
   try {
     let response = await fetch(url, {
@@ -161,4 +318,97 @@ async function newCombo(state, plate) {
     console.error(error);
     return "Error";
   }
+}
+
+function checkFormats(plate, formats) {
+	for (var i = 0; i < formats.length; i++) {
+		switch (formats[i]) {
+			case "ABC123":
+				if (/^[a-zA-Z]{3}[-\s]?\d{3}$/.test(plate)) {
+					return 1;
+				} else continue;
+			case "123ABC":
+				if (/^\d{3}[-\s]?[a-zA-Z]{3}$/.test(plate)) {
+					return 2;
+				} else continue;
+			case "ABC1234":
+				if (/^[a-zA-Z]{3}[-\s]?\d{4}$/.test(plate)) {
+					return 3;
+				} else continue;
+			case "1234ABC":
+				if (/^\d{4}[-\s]?[a-zA-Z]{3}$/.test(plate)) {
+					return 4;
+				} else continue;
+			case "123AB":
+				if (/^\d{3}[a-zA-Z]{2}$/.test(plate)) {
+					return 5;
+				} else continue;
+			case "123A":
+				if (/^\d{3}[a-zA-Z]{1}$/.test(plate)) {
+					return 6;
+				} else continue;
+			case "A1B234":
+				if (/^[a-zA-Z]{1}\d{1}[a-zA-Z]{1}\d{3}$/.test(plate)) {
+					return 7;
+				} else continue;
+			case "A123BC":
+				if (/^[a-zA-Z]{1}\d{2}[-\s]?\d{1}[a-zA-Z]{2}$/.test(plate)) {
+					return 8;
+				} else continue;
+			case "12A345":
+				if (/^\d{2}[a-zA-Z]{1}[-\s]?\d{3}$/) {
+					return 9;
+				} else continue;
+			case "123A45":
+				if (/^\d{3}[-\s]?[a-zA-Z]{1}\d{2}$/) {
+					return 10;
+				} else continue;
+			default:
+				return 0;
+		}
+	}
+	return 0;
+}
+
+function checkFormat(plate, format) {
+	switch (format) {
+		case "ABC123":
+			if (/^[a-zA-Z]{3}[-\s]?\d{3}$/.test(plate)) {
+				return 1;
+			} else return 0;
+		case "ABC1234":
+			if (/^[a-zA-Z]{3}[-\s]?\d{4}$/.test(plate)) {
+				return 2;
+			} else return 0;
+		case "123ABC":
+			if (/^\d{3}[-\s]?[a-zA-Z]{3}$/.test(plate)) {
+				return 3;
+			} else return 0;
+		case "AB1234":
+			if (/^[a-zA-Z]{2}[-\s]?\d{4}$/.test(plate)) {
+				return 4;
+			} else return 0;
+		case "1234AB":
+			if (/^\d{4}[-\s]?[a-zA-Z]{2}$/.test(plate)) {
+				return 5;
+			} else return 0;
+		case "1ABC234":
+			if (/^\d{1}[a-zA-Z]{3}\d{3}$/.test(plate)) {
+				return 6;
+			} else return 0;
+		case "1AB2345":
+			if (/^\d{1}[a-zA-Z]{2}\d{4}$/.test(plate)) {
+				return 7;
+			} else return 0;
+		case "X12 3456":
+			if (/^\w{1}\d{2}\s{1}\d{4}$/.test(plate)) {
+				return 8;
+			} else return 0;
+		default:
+			return 0;
+	}
+}
+
+function isHyphen(plate) {
+	return /[-\s]/.test(plate);
 }
