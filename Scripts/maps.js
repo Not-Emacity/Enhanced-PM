a3de24e5611cf080bccb66cd4b7f3fe3ca6c80d3
Matthew <54685753+Not-Emacity@users.noreply.github.com>
2025-05-04 19:55:29 -0500

    Update maps.js

--- Scripts/maps.js
+++ Scripts/maps.js
@@ -7,7 +7,9 @@ const makeCorrections = new Map([
 	["HYUNDAI TRANSLEAD TRAILERS", "Translead"],
 	["FONTAINE TRAILER CO.", "Fontaine"],
 	["GREAT DANE TRAILERS", "Great Dane Trailers"],
-	["SPARTAN MOTORS CHASSIS", "Spartan"]
+	["SPARTAN MOTORS CHASSIS", "Spartan"],
+	["VANDERHALL MOTOR WORKS", "Vanderhall"],
+	["INDIAN MOTORCYCLE", "Indian"]
 ]);
 
 const modelCorrections = new Map([
@@ -23,11 +25,23 @@ const modelCorrections = new Map([
 	["XJ8", "XJ"],
 	["Captiva Sport", "Captiva"],
 	["LF627", "ProStar"],
+	["LF687", "ProStar"],
 	["Bolt EV", "Bolt"],
 	["4-Runner", "4Runner"],
 	["CST120", "Century"],
 	["PB105", "CE"],
-	["YZF-R1", "YZF"]
+	["YZF-R1", "YZF"],
+	["L125 (Monkey)", "Monkey"],
+	["LA687", "ProStar"],
+	["NISSAN Z", "Z"],
+	["LT62F", "LT"],
+	["LT625", "LT"],
+	["LT62T", "LT"],
+	["Town and Country", "Town&Country"],
+	["MA025", "DuraStar"],
+	["EQB-Class", "EQB"],
+	["BB Conventional", "Vision"],
+	["USF-1E", "FLD"]
 ]);
 
 const trailerMakes = new Set([
